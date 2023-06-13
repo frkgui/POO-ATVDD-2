b0f7c8bbd2003a7e03064aa1d46d398190747b3e
frkgui <78104494+frkgui@users.noreply.github.com>
2023-06-13 14:52:05 -0300

    add moto.java & carro.java

---
+++ src/com/atividade/primeira/Moto.java
@@ -0,0 +1,8 @@
+package com.atividade.primeira;
+
+public class Moto extends Veiculo{
+    @Override
+    public void acelerar() {
+        this.velocidade += 10;
+    }
+}
---
+++ src/com/atividade/primeira/Veiculo.java
@@ -0,0 +1,10 @@
+package com.atividade.primeira;
+
+public abstract class Veiculo {
+    String marca, modelo;
+    int velocidade = 0;
+
+
+    public abstract void acelerar();
+
+}
